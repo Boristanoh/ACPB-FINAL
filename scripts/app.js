acc8757d6be434a0dbb0fef964b36630a14130d2
Boris TANOH <138991507+Boristanoh@users.noreply.github.com>
2024-01-22 14:35:16 +0000

    debut du site

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,23 @@
-var sidenav = document.getElementById("mySidenav");
+var sidenav = document.querySelector(".navbarlink")
 var openBtn = document.getElementById("openBtn");
 var closeBtn = document.getElementById("closeBtn");
+var navbar = document.querySelector(".navbar")
 
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
+  console.log(1)
+  openBtn.classList.add("ferme");
   sidenav.classList.add("active");
+  navbar.classList.add("boutton");
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   sidenav.classList.remove("active");
+
+  navbar.classList.remove("boutton");
+  openBtn.classList.remove("ferme");
 }
