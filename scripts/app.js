b5c5bd6839713869fe5ad738c04d845ede3eb371
Boris TANOH <138991507+Boristanoh@users.noreply.github.com>
2024-01-24 08:55:01 +0000

    animation

--- scripts/app.js
+++ scripts/app.js
@@ -21,3 +21,41 @@ function closeNav() {
   navbar.classList.remove("boutton");
   openBtn.classList.remove("ferme");
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const textElement = document.querySelector('.start__p2');
+  const textToAnimate = "La BCP-ST plus qu'une famille.";
+
+  const textElement2 = document.querySelector('.start__p1');
+  textElement.textContent = "";
+  const textToAnimate2 = "Association  des Classes Préparatoires Biologiques ";
+
+  function typeText(textElement, textToAnimate, callback) {
+    textElement.textContent = "";
+    let i = 0;
+
+    function type() {
+      if (i < textToAnimate.length) {
+        textElement.textContent += textToAnimate.charAt(i);
+        i++;
+        setTimeout(type, 100); // Contrôle la vitesse de frappe
+      } else {
+        // Appel de la fonction de rappel une fois que le texte est entièrement écrit
+        callback();
+      }
+    }
+
+    type();
+  }
+
+  // Première animation
+  typeText(textElement2, textToAnimate2, function () {
+    // Fonction de rappel appelée lorsque le texte 1 est entièrement écrit
+    // Démarrez la deuxième animation ici
+    typeText(textElement, textToAnimate, function () {
+      // Fonction de rappel pour la deuxième animation si nécessaire
+    });
+  });
+});
+
