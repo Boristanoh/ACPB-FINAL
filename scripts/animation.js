f5a436100930707585101075da9d2adc3cac6d72
Boris TANOH <138991507+Boristanoh@users.noreply.github.com>
2024-01-25 10:12:59 +0000

    animation

---
+++ scripts/animation.js
@@ -0,0 +1,49 @@
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Liste des classes à traiter
+
+
+  var classesToCheck = document.querySelectorAll("section");
+
+  // Écouter l'événement de scroll et appeler la fonction pour chaque classe
+  document.addEventListener("scroll", function() {
+    classesToCheck.forEach(function(className) {
+      checkVisibility(className);
+    });
+  });
+
+  // Appeler la fonction pour vérifier la visibilité au chargement du DOM pour chaque classe
+  classesToCheck.forEach(function(className) {
+    checkVisibility(className);
+  });
+});
+
+// Fonction pour vérifier la visibilité pour une classe donnée
+function checkVisibility(className) {
+//   var element = document.querySelector(className);
+  var isVisible = false; // Booléen pour suivre si l'élément est déjà visible
+
+  // Vérifier si l'élément est potentiellement visible
+  if (!isVisible) {
+    var position = className.getBoundingClientRect().top;
+    var screenHeight = window.innerHeight;
+    if (position < screenHeight * 1) {
+        // className.classList.remove(".animation-sortir-droit")
+      className.classList.add("animation-entrer-droit");
+      isVisible = true; // Marquer l'élément comme visible
+    }
+    else{
+        className.classList.remove("animation-entrer-droit");
+        // className.classList.add(".animation-sortir-droit")
+        isVisible = false;
+        
+    }
+  }
+
+}
+
+
+
