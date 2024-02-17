4a380cc55f10a4858a83d5406f486d7c9c964ef5
Boris TANOH <138991507+Boristanoh@users.noreply.github.com>
2024-02-17 16:02:38 +0000

    changement enfin

--- scripts/popup.js
+++ scripts/popup.js
@@ -73,13 +73,16 @@ function sendEmail() {
   var objet = document.getElementById('objet').value;
   var message = document.getElementById('message').value;
   var name = document.getElementById('nom').value;
+  
   // Effectuez la validation personnalisée
   if (name === '' || email === '' || objet === '' || message === '') {
       alert('Veuillez remplir tous les champs du formulaire.');
       return; // Ne continuez pas avec l'envoi si la validation échoue
   }else{
+    document.getElementById("formulaire").inert = true
+    document.getElementById("loading-icon").style.display = "block"
     
-
+    
 
   // Définissez les placeholders dynamiquement
   var templateParams = {
@@ -93,6 +96,7 @@ function sendEmail() {
   emailjs.send('service_aklc7xh', 'template_3f8dxto', templateParams)
     .then(function(response) {
         afficherPopup()
+        sendresponse(email.toLowerCase(),name)
       console.log('E-mail envoyé avec succès :', response);
       
       // Réinitialisez les champs du formulaire
@@ -100,12 +104,59 @@ function sendEmail() {
       document.getElementById('objet').value = '';
       document.getElementById('message').value = '';
       document.getElementById('nom').value = '';
+      document.getElementById("loading-icon").style.display = "none"
+      document.getElementById("formulaire").inert = false
+      
     }, function(error) {
       console.log('Erreur lors de l\'envoi de l\'e-mail :', error);
+      document.getElementById("loading-icon").style.display = "none"
+      document.getElementById("formulaire").inert = false
+      alert('Erreur lors de l\'envoi de l\'e-mail :');
       document.getElementById('error-message').textContent = 'Erreur lors de l\'envoi de l\'e-mail.';
+
     });
+    
   }
   
+}
+
+function sendresponse(ReceiveEmail,ReceiveName){
+    fetch('https://api.sendinblue.com/v3/smtp/email', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'api-key': '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'
+  },
+  body: JSON.stringify({
+    sender: { name: 'Association des Classes Préparatoires Biologiques(ACPB)', email: 'acpb.inphb@gmail.com' },
+    to: [{ email: ReceiveEmail }],
+    subject: 'Réponse automatique - Réception de votre message',
+    textContent: ` 
+     Bonjour ${ReceiveName},
+    
+    Nous vous remercions d'avoir pris le temps de nous envoyer votre avis/question. Veuillez noter que cette réponse est automatique pour confirmer la réception de votre message.
+    
+    Nous avons bien reçu votre message et nous vous en sommes reconnaissants.
+    
+    Votre contribution est précieuse pour nous et nous nous engageons à l'examiner attentivement. Nous vous fournirons une réponse détaillée dès que possible.
+    
+    En attendant, n'hésitez pas à nous contacter si vous avez d'autres questions ou préoccupations.
+    
+    Cordialement,
+    
+    Association des Classes Préparatoires Biologiques(ACPB)
+
+    E-mail: acpb.inphb@gmail.com`
+  })
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Erreur lors de l\'envoi de l\'e-mail');
+  }
+  console.log('E-mail envoyé avec succès');
+})
+.catch(error => console.error('Erreur:', error));
+
 }
 // Initialisez Email.js avec votre User ID
 emailjs.init("ebo48V2ej9i53Mrb7");
